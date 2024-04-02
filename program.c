363346793bae4b1a352789b7fb6dfa0d5df96da1
khushi011324 <145763449+khushi011324@users.noreply.github.com>
2024-04-02 12:39:31 +0530

    Update program.c

--- program.c
+++ program.c
@@ -6,89 +6,72 @@
 using namespace std;
 using namespace std::chrono;
 
-// Function to generate random matrix
-void randomMatrix(int* matrix, int rows, int cols) {
-    for (int i = 0; i < rows * cols; ++i) {
-        matrix[i] = rand() % 100;
-    }
-}
-
-// Function to multiply matrices
-void matrixMultiply(int* A, int* B, int* C, int rowsA, int colsA, int colsB, int startRow, int endRow) {
-    for (int i = startRow; i < endRow; ++i) {
-        for (int j = 0; j < colsB; ++j) {
-            C[i * colsB + j] = 0;
-            for (int k = 0; k < colsA; ++k) {
-                C[i * colsB + j] += A[i * colsA + k] * B[k * colsB + j];
-            }
-        }
+void randomVector(int* vector, int size) {
+    for (int i = 0; i < size; i++) {
+        vector[i] = rand() % 100;
     }
 }
 
 int main(int argc, char** argv) {
-    int rank, size;
-    int rowsA = 1000; // Number of rows in matrix A
-    int colsA = 1000; // Number of columns in matrix A
-    int colsB = 1000; // Number of columns in matrix B
-    int* A, * B, * C;
-    int* localA, * localC;
-
-    srand(time(NULL));
+    unsigned long size = 100000000;
+    srand(time(0));
+    int* v1 = nullptr, * v2 = nullptr, * v3 = nullptr;
 
     MPI_Init(&argc, &argv);
+    int rank, num_procs;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    // Calculate number of rows to be processed by each process
-    int rowsPerProcess = rowsA / size;
-    int remainingRows = rowsA % size;
+    // Calculate the size of the vector for each process
+    unsigned long local_size = size / num_procs;
 
-    // Allocate memory for matrices B and localC
-    B = new int[colsA * colsB];
-    localC = new int[rowsPerProcess * colsB];
-
-    // Allocate memory for matrix A in root process
+    // Allocate memory for local vectors in root process
     if (rank == 0) {
-        A = new int[rowsA * colsA];
-        randomMatrix(A, rowsA, colsA);
-        // Allocate memory for receive buffer in root process
-        C = new int[rowsA * colsB];
+        v1 = new int[size];
+        v2 = new int[size];
+        v3 = new int[size];
+        randomVector(v1, size);
+        randomVector(v2, size);
     }
 
-    // Allocate memory for localA based on rank
-    localA = new int[rowsPerProcess * colsA];
+    // Allocate memory for local vectors in each process
+    int* local_v1 = new int[local_size];
+    int* local_v2 = new int[local_size];
+    int* local_v3 = new int[local_size];
 
-    // Scatter rows of matrix A to all processes
-    MPI_Scatter(A, rowsPerProcess * colsA, MPI_INT, localA, rowsPerProcess * colsA, MPI_INT, 0, MPI_COMM_WORLD);
-
-    // Broadcast matrix B to all processes
-    MPI_Bcast(B, colsA * colsB, MPI_INT, 0, MPI_COMM_WORLD);
+    // Scatter data to all processes
+    MPI_Scatter(v1, local_size, MPI_INT, local_v1, local_size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(v2, local_size, MPI_INT, local_v2, local_size, MPI_INT, 0, MPI_COMM_WORLD);
 
     auto start = high_resolution_clock::now();
 
-    // Perform matrix multiplication
-    matrixMultiply(localA, B, localC, rowsA, colsA, colsB, 0, rowsPerProcess);
+    // Perform computation locally
+    for (unsigned long i = 0; i < local_size; i++) {
+        local_v3[i] = local_v1[i] + local_v2[i];
+    }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
-    // Gather the results from all processes to rank 0
-    MPI_Gather(localC, rowsPerProcess * colsB, MPI_INT, C, rowsPerProcess * colsB, MPI_INT, 0, MPI_COMM_WORLD);
+    // Gather results back to the root process
+    MPI_Gather(local_v3, local_size, MPI_INT, v3, local_size, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Print the time taken for matrix multiplication
+    // Print time taken by root process
     if (rank == 0) {
-        cout << "Time taken for matrix multiplication: " << duration.count() << " microseconds" << endl;
+        cout << "MPI multiplication time: " << duration.count() << " microseconds" << endl;
     }
 
     // Deallocate memory
-    delete[] A;
-    delete[] B;
-    delete[] localA;
-    delete[] localC;
     if (rank == 0) {
-        delete[] C;
+        delete[] v1;
+        delete[] v2;
+        delete[] v3;
     }
+    delete[] local_v1;
+    delete[] local_v2;
+    delete[] local_v3;
 
     MPI_Finalize();
+
     return 0;
 }
